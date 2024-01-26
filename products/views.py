6e7867bd9642956dda1822121b9c9453b5fc3194
ShayanHosseinzadeh <shayanhosseinzadeh1@gmail.com>
2024-01-26 22:22:50 +0330

    fixed bug of manager

--- products/views.py
+++ products/views.py
@@ -15,16 +15,12 @@ class ProductListView(generic.ListView):
     def get_queryset(self):
         return Product.objects.filter(status='avl').order_by('-price')
 
-
+#
 def ProductDetailView(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    comments = Comment.verified_comments.order_by('-datetime_created')
+    comments = Comment.verified_comments.filter(product=product).order_by('-datetime_created')
     comment_form = CommentForm()
-    return render(request, 'products/product_detail.html',
-                  {'product': product,
-                   'comments': comments,
-                   'comment_form': comment_form}
-                  )
+
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -32,31 +28,11 @@ def ProductDetailView(request, pk):
             comment.product = product
             comment.user = request.user
             comment.save()
+        else:
+            comment_form = CommentForm()
 
-# class ProductDetailView(generic.DetailView):
-#     model = Product
-#     template_name = 'products/product_detail.html'
-#     context_object_name = 'product'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['comment_form'] = CommentForm()
-#         return context
-
-#
-# class CommentCreateView(generic.CreateView):
-#     model = Comment
-#     form_class = CommentForm
-#     def get_success_url(self):
-#         return reverse('product_list')
-#
-#     def form_valid(self, form):
-#         comment = form.save(commit=False)
-#         comment.user = self.request.user
-#
-#         product_id = int(self.kwargs['product_id'])
-#         product = get_object_or_404(Product, id=product_id)
-#
-#         comment.product = product
-#         return super().form_valid(form)
-#
+    return render(request, 'products/product_detail.html',
+                  {'product': product,
+                   'comments': comments,
+                   'comment_form': comment_form}
+                  )
