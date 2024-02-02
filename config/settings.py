ad3fcad827c5d0992370f8a94d89b7086cf6f919
ShayanHosseinzadeh <shayanhosseinzadeh1@gmail.com>
2024-02-02 14:45:20 +0330

    Added persian numbers and commas

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
 
     # third party
     'allauth',
@@ -51,6 +53,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
+    'persian_translate.apps.PersianTranslateConfig',
 
 
 ]
@@ -147,6 +150,7 @@ LOCALE_PATHS = (
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
