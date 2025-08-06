86ecd546ecab7370c4d0477edd659c642ad4bd66
Shayan Hosseinzadeh <shayanhosseinzadeh1@gmail.com>
2025-08-06 11:53:27 +0330

    Finished cart

--- cart/cart.py
+++ cart/cart.py
@@ -4,58 +4,53 @@ from products.models import Product
 
 
 class Cart:
-    """
-    Initialize the session and the cart
-    """
     def __init__(self, request):
         self.request = request
         self.session = request.session
         cart = self.session.get('cart')
         if not cart:
-            cart = request.session['cart'] = {}
+            cart = self.session['cart'] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1,replace_current_quantity=False):
-        """
-        Add a product to the cart or update the cart
-        """
+    def add(self, product, quantity=1, replace_current_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0}
+
         if replace_current_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
+
         messages.success(self.request, _('Product added successfully'))
         self.save()
 
     def remove(self, product):
-        """
-        Remove a product from the cart
-        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             messages.success(self.request, _('Product removed successfully'))
-
             self.save()
 
     def save(self):
-        """
-        update session
-        """
         self.session.modified = True
 
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product_obj'] = product
+        products_map = {str(product.id): product for product in products}
 
-        for item in cart.values():
-            item['total_price'] = item['quantity'] * item['product_obj'].price
-            yield item
+        for product_id, item in self.cart.items():
+            product = products_map.get(product_id)
+            if product:
+                item = item.copy()
+                item['product_obj'] = product
+                if product.discount_percent > 0:
+                    discounted_price = product.price * (100 - product.discount_percent) // 100
+                else:
+                    discounted_price = product.price
+                item['total_price'] = item['quantity'] * discounted_price
+                yield item
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
@@ -66,4 +61,26 @@ class Cart:
 
     def get_total(self):
         product_ids = self.cart.keys()
-        return sum(item['quantity'] * item['product_obj'].price for item in self.cart.values())
+        products = Product.objects.filter(id__in=product_ids)
+        total = 0
+        for product in products:
+            quantity = self.cart[str(product.id)]['quantity']
+            if product.discount_percent > 0:
+                discounted_price = product.price * (100 - product.discount_percent) // 100
+            else:
+                discounted_price = product.price
+            total += quantity * discounted_price
+        return total
+
+
+    def get_total_discount(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        total_discount = 0
+        for product in products:
+            quantity = self.cart[str(product.id)]['quantity']
+            if product.discount_percent > 0:
+                original_total = quantity * product.price
+                discounted_total = quantity * (product.price * (100 - product.discount_percent) // 100)
+                total_discount += original_total - discounted_total
+        return total_discount
--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from django.views.decorators.http import require_http_methods
 
 from .forms import CartAddForm
 from products.models import Product
@@ -22,18 +23,29 @@ def CartDetailView(request):
     return render(request, 'cart/cart_detail.html', {'cart': cart})
 
 
-@require_POST
+@require_http_methods(["GET", "POST"])
 def AddToCart(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddForm(request.POST)
-    if form.is_valid():
-        cleaned_data = form.cleaned_data
-        quantity = cleaned_data['quantity']
-        cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
+
+    if request.method == 'POST':
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            quantity = cleaned_data['quantity']
+            cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
+    else:
+        action = request.GET.get('action')
+        current_quantity = cart.cart.get(str(product_id), {}).get('quantity', 0)
+
+        if action == 'increase':
+            cart.add(product, quantity=1)
+        elif action == 'decrease' and current_quantity > 1:
+            cart.add(product, quantity=current_quantity - 1, replace_current_quantity=True)
+        elif action == 'decrease' and current_quantity == 1:
+            cart.remove(product)
 
     return redirect('cart_detail')
-
 
 def removecart(request, product_id):
     cart = Cart(request)
@@ -56,7 +68,6 @@ def clear_cart(request):
 
 @require_POST
 def update_quantity_htmx(request):
-    print('hi')
     cart = Cart(request)
     product_id = request.POST.get('product_id')
     action = request.POST.get('action')
@@ -71,16 +82,16 @@ def update_quantity_htmx(request):
     elif action == 'decrease' and current_quantity == 1:
         cart.remove(product)
 
-    # بررسی کن ببینی هنوز توی سبد هست یا نه
+
     item = next((i for i in cart if i['product_obj'].id == product.id), None)
 
     if item:
         item['product_quantity_update_form'] = CartAddForm(
             initial={'quantity': item['quantity'], 'inplace': True}
         )
+        print(item)
         html = render_to_string('cart/partials/cart_item.html', {'item': item}, request=request)
         return HttpResponse(html)
     else:
-        # اگه حذف شده، یک بلاک خالی برگردون
         return HttpResponse("")
 
