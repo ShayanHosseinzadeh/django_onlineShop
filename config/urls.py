859a22b893b19b983cd10b72694a65ab9fbe5210
Shayan Hosseinzadeh <shayanhosseinzadeh1@gmail.com>
2025-08-08 16:25:37 +0330

    Added home page

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('rosetta/', include('rosetta.urls')),
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.views import generic
+from products.models import Product, Category
+
+
 # Create your views here.
+
 class HomePageView(generic.TemplateView):
-    template_name = 'home.html'
+    template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        colors = [
+            "bg-blue-500", "bg-green-500", "bg-purple-500",
+            "bg-pink-500", "bg-yellow-500", "bg-red-500"
+        ]
+        hover_colors = [
+            "bg-blue-600", "bg-green-600", "bg-purple-600",
+            "bg-pink-600", "bg-yellow-600", "bg-red-600"
+        ]
+
+        categories = Category.objects.all()
+        for i, cat in enumerate(categories):
+            cat.color_class = colors[i % len(colors)]
+            cat.hover_color = hover_colors[i % len(hover_colors)]
+
+        context['categories'] = categories
+        context['products'] = Product.objects.all()
+        context['discounted_products'] = Product.objects.filter(discount_percent__gt=0).order_by('-discount_percent')[:4]
+        return context
+
 
 class AboutUsPageView(generic.TemplateView):
     template_name = 'pages/aboutus.html'
-
