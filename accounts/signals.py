ea19b980f5ef3d28edeff0f22b58e05a95f55bc1
Shayan Hosseinzadeh <shayanhosseinzadeh1@gmail.com>
2025-08-11 14:55:47 +0330

    Added form errors to signup form + fixed signal of making account

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,11 +1,15 @@
+# users/signals.py (or accounts/signals.py)
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import UserProfile
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     else:
-        instance.userprofile.save()
+        instance.profile.save()
